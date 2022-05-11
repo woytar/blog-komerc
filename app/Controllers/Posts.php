dd68337250997c0625a4f5f076a5b4e956d07c6b
woytar <maciej.wojtarek@gmail.com>
2022-05-11 20:18:14 +0200

    Initial commit

---
+++ app/Controllers/Posts.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+class Posts extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+
+    public function view($page = 'home')
+{
+    if (! is_file(APPPATH . 'Views/posts/' . $page . '.php')) {
+        // Whoops, we don't have a page for that!
+        throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+    }
+
+    $data['title'] = ucfirst($page); // Capitalize the first letter
+
+    echo view('templates/header', $data);
+    echo view('posts/' . $page, $data);
+    echo view('templates/footer', $data);
+}
+}
---
+++ app/Views/posts/home.php
@@ -0,0 +1,1 @@
+Home tekst <br>
